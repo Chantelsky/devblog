cb5018d17cec9fff48f855b0d8d800f08bcae498
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-12-11 07:53:41 +1000

    re-formatting the sidebar

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,17 +6,23 @@ import './nav.module.scss'
 
 const Header = () => {
     return (
-        <nav className="menu">
-            <header className="avatar">
-                <img src={avatar}></img>
-                <h2>Chantel Sky</h2>
-            </header>
-            <ul>
-                <li><Link to="/">Blog</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="https://chantelsky.netlify.com" target="_blank" norel="noopener">Portfolio</Link></li>
-            </ul>
-        </nav >
+        <div className="sidebar">
+            <nav>
+                <div className="avatar">
+                    <img src={avatar}></img>
+                    <h2>Chantel Sky</h2>
+                </div>
+                <ul>
+                    <li>hi</li>
+                </ul>
+            </nav>
+            <button>Follow @Chantelsky</button>
+            <div className="social-media">
+                <ul>
+                    <li>hello</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
